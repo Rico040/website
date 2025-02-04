ae62ce3bc072b2d7ae91709211873556dbeeb1ff
PalmDevs <git@palmdevs.me>
2025-02-04 19:55:31 +0700

    fix: update theme color and remove redundant favicons

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -18,9 +18,8 @@ export default createHandler(() => {
                     <head>
                         <meta charset="utf-8" />
                         <meta name="viewport" content="width=device-width, initial-scale=1" />
-                        <meta name="theme-color" content="#4ac9ff" />
+                        <meta name="theme-color" content="#509cff" />
                         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-                        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#006fe9" />
                         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
                         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
                         <link rel="icon" href="/favicon.ico" />
